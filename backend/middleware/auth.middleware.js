07c758ea16225a112cb18796d5fb393de9869088
Laura Kazikhanova <anarab902@gmail.com>
2026-01-27 10:17:13 +0500

    updated server.js

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,14 +1,12 @@
 const jwt = require('jsonwebtoken');
-
-const SECRET = process.env.JWT_SECRET || 'MENTAL_HEALTH_SECRET';
+const SECRET = process.env.JWT_SECRET;
 
 module.exports = function auth(req, res, next) {
   const header = req.headers.authorization;
-
   if (!header) {
     return res.status(401).json({ message: 'No token provided' });
   }
-
+  
   const token = header.split(' ')[1];
   if (!token) {
     return res.status(401).json({ message: 'Invalid token format' });
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// require('dotenv').config();
-
-// const authRoutes = require('./routes/auth.routes');
-// const surveyRoutes = require('./routes/survey.routes');
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/surveys', surveyRoutes);
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error(err));
-
-// app.listen(3000, () => {
-//   console.log('Server running on http://localhost:3000');
-// });
-
-
 require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
